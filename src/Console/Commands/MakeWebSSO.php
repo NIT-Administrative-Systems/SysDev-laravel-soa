adb2957c6b52a18d118871c46ae3c84241a41867
Nick Evans <nick.evans@northwestern.edu>
2019-10-10 11:58:16 -0500

    Eject SSO assets & routes

--- src/Console/Commands/MakeWebSSO.php
+++ src/Console/Commands/MakeWebSSO.php
@@ -3,10 +3,11 @@
 namespace Northwestern\SysDev\SOA\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Facades\Artisan;
 
 class MakeWebSSO extends GeneratorCommand
 {
-    protected $name = 'make:websso';
+    protected $signature = 'make:websso';
     protected $description = 'Create a new Northwestern WebSSO controller';
     protected $type = 'Controller';
 
@@ -14,17 +15,34 @@ class MakeWebSSO extends GeneratorCommand
     {
         parent::handle();
 
-        // GeneratorCommand only does one stub at a time, but I want both, so...
+        // GeneratorCommand only does one stub at a time, but I want both, so chaining these
+        // is an expedient way of writing this cleanly!
         Artisan::call(MakeDuo::class);
+
+        $this->ejectRoutes();
+    }
+
+    protected function getNameInput()
+    {
+        return 'WebSSOController';
     }
 
     protected function getStub()
     {
-        return __DIR__ . '/../../stubs/WebSSOController.stub';
+        return __DIR__ . '/../../../stubs/WebSSOController.stub';
     }
 
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace . '\Http\Controllers\Auth';
+    }
+
+    protected function ejectRoutes()
+    {
+        file_put_contents(
+            base_path('routes/web.php'),
+            file_get_contents(__DIR__.'/../../../stubs/routes.stub'),
+            FILE_APPEND
+        );
     }
 }
