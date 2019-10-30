5d685a4c5149dd05d351356b18be17e70355a2f2
n8ta <n8@u.northwestern.edu>
2019-10-30 13:28:30 -0500

    Delete between 1000 and 5000 access logs when we are above the max set in the ENV (default = 1mil)

--- src/Http/Middleware/SsoLogger.php
+++ src/Http/Middleware/SsoLogger.php
@@ -2,13 +2,17 @@
 
 namespace Northwestern\SysDev\SOA\Http\Middleware;
 
-use http\Env;
+use http\ENV;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
 use Auth;
+use App\Access;
 use Closure;
 
 class SsoLogger extends Middleware
 {
+    protected $max_logs;
+
+    public function __construct() { $this->max_logs = ENV('SSO_LOG_MAX_ENTRIES',1000000); }
 
     public function handle($request, Closure $next, ...$guards)
     {
@@ -27,23 +31,36 @@ class SsoLogger extends Middleware
                 $this->log_request(false, $request);
             }
         }
+
+
+        if (sizeof(Access::all()) > $this->max_logs) {
+            $this->remove_oldest_logs();
+        };
+
         return $next($request);
     }
 
     private function log_request($anonymous, $request) {
         $path = $request->path();
-
         try {
             $agent = $request->header('User-Agent');
         }  catch (Exception $x) {
             $agent = null;
         }
 
-        $access = new \App\Access(['path'=>$path,'agent'=>$agent]);
+        $access = new Access(['path'=>$path,'agent'=>$agent]);
         if (!$anonymous) {
             $access->netid = Auth::user()['netid'];
         }
         $access->save();
         return;
+    }
+
+    private function remove_oldest_logs() {
+        // Remove between 1000 and 5000 logs at a time. Scaled roughly by max logs set.
+        // I don't want to remove more than 5000 and risk slowing down the request significantly.
+        $num_to_remove = max(min(5000,$this->max_logs/10),1000);
+        Access::orderBy('created_at','asc')->take($num_to_remove)->get()->each->delete();
+        # ^ Probably a better way to do this than individual deletes
     }
 }
