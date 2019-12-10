da93eb7cae46c17567f4680db9c6520316821671
Nick Evans <nick.evans@northwestern.edu>
2019-12-10 17:19:33 -0600

    Replace WebSSO w/ interface & implement OpenAM APIs

--- src/WebSSO.php
+++ src/WebSSO.php
@@ -2,90 +2,21 @@
 
 namespace Northwestern\SysDev\SOA;
 
-use GuzzleHttp;
+use Northwestern\SysDev\SOA\WebSSOImpl\User;
 
-/**
- * OpenAM webSSO API bindings for agentless authentication.
- */
-class WebSSO
+interface WebSSO
 {
-    protected $sso_server;
-    protected $api_path = '/amserver/identity/attributes';
-    protected $http_client;
-
-    public function __construct(GuzzleHttp\Client $client)
-    {
-        $this->http_client = $client;
-        $this->sso_server = config('nusoa.sso.openAmBaseUrl');
-    } // end __construct
+    public function getUser(string $token): ?User;
+    public function getLoginUrl(string $redirect_path): string;
+    public function getLogoutUrl(): string;
+    public function getCookieName(): string;
 
     /**
-     * Turns an openAMssoToken cookie into a netID.
+     * Gets the netID for a token.
      *
-     * @param  string value of openAMssoToken cookie
-     * @return string   the netID, or false for an invalid token.
+     * Deprecated -- use getUser() instead; it returns additional information.
+     *
+     * @deprecated 4.0.0
      */
-    public function getNetID($token)
-    {
-        $request = $this->http_client->request('POST', $this->getEndpointUrl(), [
-            'http_errors' => false, // don't throw exceptions
-            'form_params' => [
-                'subjectid' => $token,
-                'attributenames' => 'UserToken',
-            ],
-        ]);
-
-        if ($request === null) {
-            throw new \Exception(vsprintf('Unable to reach webSSO service. Verify connectivity to %s', [$this->getEndpointUrl()]));
-        }
-
-        if ($request->getStatusCode() != 200) {
-            return false;
-        }
-
-        return $this->extractNetid($request->getBody());
-    } // end getInfo
-
-    public function setHttpClient(GuzzleHttp\Client $client)
-    {
-        $this->http_client = $client;
-    } //end setHttpClient
-
-    protected function extractNetid($payload)
-    {
-        $seek = 'userdetails.attribute.value=';
-        foreach (explode("\n", $payload) as $line) {
-            if (substr($line, 0, strlen($seek)) == $seek) {
-                return trim(substr($line, strlen($seek)));
-            }
-        }
-
-        return null;
-    } // end extractNetid
-
-    protected function getEndpointUrl()
-    {
-        return $this->sso_server . $this->api_path;
-    } // end getEndpointUrl
-
-    /**
-     * [getLoginUrl description]
-     * @param  [type] $redirect_to [description]
-     * @return [type]              [description]
-     */
-    public function getLoginUrl($redirect_path = null)
-    {
-        $redirect_to = config('app.url');
-        if ($redirect_path != null) {
-            $redirect_to = vsprintf('%s/%s', [trim($redirect_to, '/'), trim($redirect_path, '/')]);
-        }
-
-        $redirect_to = urlencode($redirect_to);
-        return $this->sso_server . "/amserver/UI/Login?goto=$redirect_to&ForceAuthn=false&IsPassive=false&Federate=false";
-    } // end getLoginUrl
-
-    public function getLogoutUrl()
-    {
-        return $this->sso_server . "/amserver/UI/Logout.jsp";
-    } // end getLogoutUrl
-} // end WebSSO
+    public function getNetID(string $token): ?string;
+}
---
+++ src/WebSSOImpl/OpenAM11Api.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Northwestern\SysDev\SOA\WebSSOImpl;
+
+use GuzzleHttp;
+use Northwestern\SysDev\SOA\WebSSO;
+
+class OpenAM11Api implements WebSSO
+{
+    protected $app_url;
+    protected $http_client;
+    protected $sso_server;
+    protected $realm;
+    protected $tree;
+    protected $cookie_name;
+
+    public function __construct(GuzzleHttp\Client $client, string $app_url, array $config)
+    {
+        $this->app_url = $app_url;
+        $this->http_client = $client;
+
+        $this->sso_server = $config['openAmBaseUrl'];
+        $this->realm = $config['realm'];
+        $this->tree = $config['authTree'];
+        $this->cookie_name = $config['cookieName'];
+    }
+
+    public function getUser(string $token): ?User
+    {
+        $response = $this->http_client->post($this->getEndpointUrl(), [
+            // No exceptions, we do our own error handling
+            'http_errors' => false,
+            'headers' => [
+                'Accept-API-Version' => 'resource=3',
+            ],
+            'json' => [
+                'tokenId' => $token,
+                'realm' => '/',
+            ],
+        ]);
+
+        if ($response === null) {
+            throw new \Exception(vsprintf('Unable to reach webSSO service. Verify connectivity to %s', [$this->getEndpointUrl()]));
+        }
+
+        if ($response->getStatusCode() != 200) {
+            return null;
+        }
+
+        $session = json_decode($response->getBody()->getContents(), JSON_OBJECT_AS_ARRAY);
+        return new User($session['username'], $session['properties']['isDuoAuthenticated']);
+    }
+
+    /** @deprecated 4.0.0 */
+    public function getNetID(string $token): ?string
+    {
+        $user = $this->getUser($token);
+
+        if ($user !== null) {
+            return $user->getNetid();
+        }
+
+        return null;
+    }
+
+    public function getLoginUrl(string $redirect_path): string
+    {
+        $redirect_to = $this->app_url;
+        if ($redirect_path != null) {
+            $redirect_to = vsprintf('%s/%s', [trim($redirect_to, '/'), trim($redirect_path, '/')]);
+        }
+        
+        $redirect_to = urlencode($redirect_to);
+        $login_url = sprintf('%s/nusso/XUI/?realm=%s#login&authIndexType=service&authIndexValue=%s&goto=%s', $this->sso_server, $this->realm, $this->tree, $redirect_to);
+
+        return $login_url;
+    }
+
+    
+    public function getLogoutUrl(): string
+    {
+        return sprintf('%s/nusso/json/realms/root/realms/%s/sessions?_action=logout', $this->sso_server, $this->realm);
+    }
+
+    public function getCookieName(): string
+    {
+        return $this->cookie_name;
+    }
+
+    private function getEndpointUrl(): string
+    {
+        return sprintf('%s/nusso/json/realms/root/realms/%s/sessions?_action=getSessionInfo', $this->sso_server, $this->realm);
+    }
+}
