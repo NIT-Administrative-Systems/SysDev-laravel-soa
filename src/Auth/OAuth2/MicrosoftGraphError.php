40234394c9f156dd93c982beee27931cdd800203
Nick Evans <nick.evans@northwestern.edu>
2021-06-11 11:27:40 -0500

    Log graph API failures better

---
+++ src/Auth/OAuth2/MicrosoftGraphError.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Northwestern\SysDev\SOA\Auth\OAuth2;
+
+use \Exception;
+use GuzzleHttp\Exception\ClientException;
+
+/**
+ * Unpacks the Microsoft Graph API response JSON into the full message.
+ *
+ * These errors would typically be truncated in a logger since the response body is
+ * cut off after X characters. This exception wraps a Guzzle error and exposes the full
+ * JSON response as the error message instead of truncating it.
+ */
+class MicrosoftGraphError extends Exception
+{
+    public function __construct(ClientException $previous)
+    {
+        parent::__construct($previous->getResponse()->getBody()->getContents(), $previous->getCode(), $previous);
+    }
+}
